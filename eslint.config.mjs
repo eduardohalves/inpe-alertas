4c781205d01c89169e59f9a38de80026a88d2bb1
Matsuke <eduardohenriquealvess@gmail.com>
2026-04-11 11:03:00 -0300

    Adicionado ambiente jest ao eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,20 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import jestPlugin from 'eslint-plugin-jest';
 
 export default [
   js.configs.recommended,
+  {
+    files: ['tests/**/*.test.js'], // Aplica especificamente aos seus testes [cite: 42, 59]
+    plugins: {
+      jest: jestPlugin,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.jest, // Isso define 'test' e 'expect' como válidos [cite: 49, 50]
+      },
+    },
+  },
   {
     languageOptions: {
       globals: {
